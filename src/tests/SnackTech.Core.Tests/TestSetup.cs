c0ee6f840a59c20fd2b5588331fe19e7045bd6a4
Dayvid Ribeiro Correia <dayvid@sorteonline.com.br>
2025-02-06 08:41:34 -0300

    segunda tentativa de correção de encoding

---
+++ src/tests/SnackTech.Core.Tests/TestSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace SnackTech.Orders.Core.Tests
+{
+    public class TestSetup
+    {
+        static TestSetup()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+    }
+}
