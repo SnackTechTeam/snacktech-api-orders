c86d8ba8719db17dc3dac91e8183df59dc379116
Dayvid Ribeiro Correia <dayvid@sorteonline.com.br>
2025-02-17 00:55:55 -0300

    tentando resolver problema de cobertura com merge

--- src/infra.persistence/SnackTech.Orders.Driver.DataBase/Migrations/20250202163454_Initial.cs
+++ src/infra.persistence/SnackTech.Orders.Driver.DataBase/Migrations/20250202163454_Initial.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace SnackTech.Orders.Driver.DataBase.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class Initial : Migration
     {
         /// <inheritdoc />
